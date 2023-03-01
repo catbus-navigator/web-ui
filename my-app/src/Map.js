332d6c3ffdef77e445a77c02905d792efe9e0b1c
Carter Janse <73023683+cjanse@users.noreply.github.com>
2023-03-01 16:40:18 -0500

    walking directions generated between 2 points

--- my-app/src/Map.js
+++ my-app/src/Map.js
@@ -9,6 +9,10 @@ export default function Map() {
   const [lng, setLng] = useState(-82.8354);
   const [lat, setLat] = useState(34.6767);
   const [zoom, setZoom] = useState(14);
+  const [startingAddressLat, setStartingAddressLat] = useState(-82.84463)
+  const [startingAddressLon, setStartingAddressLon] = useState(34.67919)
+  const [endingAddressLat, setEndingAddressLat] = useState(-82.83889)
+  const [endingAddressLon, setEndingAddressLon] = useState(34.67801)
 
   useEffect(() => {
     if (map.current) return; // initialize map only once
@@ -18,6 +22,35 @@ export default function Map() {
       center: [lng, lat],
       zoom: zoom
     });
+    fetch('https://api.mapbox.com/directions/v5/mapbox/walking/'+ startingAddressLat +'%2C' + startingAddressLon +'%3B' + endingAddressLat + '%2C'+ endingAddressLon + '?alternatives=false&continue_straight=true&geometries=geojson&language=en&overview=simplified&steps=true&access_token='+mapboxgl.accessToken)
+            .then(response => response.json())
+            .then(data => {
+                map.current.addSource('route', {
+                  'type': 'geojson',
+                  'data': {
+                  'type': 'Feature',
+                  'properties': {},
+                  'geometry': {
+                  'type': 'LineString',
+                  'coordinates': data.routes[0].geometry.coordinates
+                  }
+                  }
+                });
+                map.current.addLayer({
+                  'id': 'route',
+                  'type': 'line',
+                  'source': 'route',
+                  'layout': {
+                  'line-join': 'round',
+                  'line-cap': 'round'
+                  },
+                  'paint': {
+                  'line-color': '#888',
+                  'line-width': 8
+                  }
+                  });
+            })
+            .catch(error => console.log(error));
   });
 
   return (
