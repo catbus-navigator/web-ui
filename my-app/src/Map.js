54b44a901a91dc95cfe90d81d53916e8f5303724
John Turner <johnpturner8@gmail.com>
2023-03-05 15:58:35 -0500

    Fixed an error

--- my-app/src/Map.js
+++ my-app/src/Map.js
@@ -33,36 +33,35 @@ export default function Map() {
     };
 
     useEffect(() => {
-    if (map.current) return; // initialize map only once
-    map.current = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: 'mapbox://styles/mapbox/streets-v12',
-      center: [lng, lat],
-      zoom: zoom
-    });
+        if (map.current) return; // initialize map only once
+        map.current = new mapboxgl.Map({
+        container: mapContainer.current,
+        style: 'mapbox://styles/mapbox/streets-v12',
+        center: [lng, lat],
+        zoom: zoom
+        });
 
-    // Add navigation control to the map
-    map.current.addControl(new mapboxgl.NavigationControl({
-        showZoom: true,
-        showCompass: false,
-        showCurrentLocation: true, // this enables the "Show my location" icon
-        positionOptions: {
-            enableHighAccuracy: true // enable high accuracy for better location accuracy
-        },
-        visualizePitch: true
-    }));
+        // Add navigation control to the map
+        map.current.addControl(new mapboxgl.NavigationControl({
+            showZoom: true,
+            showCompass: false,
+            showCurrentLocation: true, // this enables the "Show my location" icon
+            positionOptions: {
+                enableHighAccuracy: true // enable high accuracy for better location accuracy
+            },
+            visualizePitch: true
+        }));
 
-      // Add geolocate control to the map
-      map.current.addControl(new mapboxgl.GeolocateControl({
-          positionOptions: {
-              enableHighAccuracy: true // enable high accuracy for better location accuracy
-          },
-          fitBoundsOptions: {
-              maxZoom: 15 // set a maximum zoom level when fitting the bounds
-          }
-      }));
-
-  }, []);
+        // Add geolocate control to the map
+        map.current.addControl(new mapboxgl.GeolocateControl({
+            positionOptions: {
+                enableHighAccuracy: true // enable high accuracy for better location accuracy
+            },
+            fitBoundsOptions: {
+                maxZoom: 15 // set a maximum zoom level when fitting the bounds
+            }
+        }));
+    }, []);
 
     // Effect hook to fetch the bus routes data from the API
     useEffect(() => {
@@ -165,21 +164,23 @@ export default function Map() {
 
     }, [busStops])
 
-    //Routes
     useEffect(() => {
-        map.addLayer({
-            id: 'bus-route',
-            type: 'line',
-            source: {
-                type: 'geojson',
-                data: geojson
-            },
-            paint: {
-                'line-color': '#f00', // Replace with your desired line color
-                'line-width': 5 // Replace with your desired line width
-            }
+        //Routes
+        map.current.on('load', () => {
+            map.current.addLayer({
+                id: 'bus-route',
+                type: 'line',
+                source: {
+                    type: 'geojson',
+                    data: geojson
+                },
+                paint: {
+                    'line-color': '#f00', // Replace with your desired line color
+                    'line-width': 5 // Replace with your desired line width
+                }
+            });
         });
-    });
+    }, [])
 
   return (
     <div className='Map'>
