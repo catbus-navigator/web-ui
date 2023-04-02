90a6230a4bae64bd8cd9443c72e04005b54549c7
John Turner <johnpturner8@gmail.com>
2023-04-02 14:48:31 -0400

    Apply prettier

--- my-app/src/GenerateDirections.js
+++ my-app/src/GenerateDirections.js
@@ -1,25 +1,21 @@
-import React, { useRef, useEffect, useState} from 'react';
- 
+import React, { useRef, useEffect, useState } from "react";
+
 // TO MAKE THE MAP APPEAR YOU MUST
 // ADD YOUR ACCESS TOKEN FROM
 // https://account.mapbox.com
 const accessToken = process.env.REACT_APP_MAPBOX_KEY;
 
 export default function GenerateDirections() {
-
   const [startingAddress, setStartingAddress] = useState("");
   const [endingAddress, setEndingAddress] = useState("");
 
-  var MapBoxDirections = require('@mapbox/mapbox-gl-directions');
+  var MapBoxDirections = require("@mapbox/mapbox-gl-directions");
 
   var directions = new MapboxDirections({
-    accessToken: 'YOUR-MAPBOX-ACCESS-TOKEN',
-    unit: 'metric',
-    profile: 'mapbox/cycling'
-  }); 
-  
-  return (
-    <>
-    </>
-  );
+    accessToken: "YOUR-MAPBOX-ACCESS-TOKEN",
+    unit: "metric",
+    profile: "mapbox/cycling",
+  });
+
+  return <></>;
 }
